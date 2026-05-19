ae4612f0f2e9f1663b9a814661eb4c6efc380440
noojam <kangbh0603@gmail.com>
2026-05-19 17:51:22 +0900

    initial commit

---
+++ Source/UnrealStudy/Private/RotatingActor.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RotatingActor.h"
+
+// Sets default values
+ARotatingActor::ARotatingActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ARotatingActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARotatingActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AddActorLocalRotation(RotationSpeed * DeltaTime);
+}
+
